2558bb3e298308599502d48fc818027e311cb738
Kefan <102143747+Levoyage@users.noreply.github.com>
2024-04-06 23:41:40 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,14 +2,13 @@ from flask import Flask, request, send_file, jsonify
 import pandas as pd
 import matplotlib.pyplot as plt
 import io
-from matplotlib import rcParams
-import pytz
-from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
 
 csv_url = 'https://public-paws.wmcloud.org/User:Pablo%20(WMF)/outreachy/round28/features_scores_climatechange_2022.csv.zip'
+
 
 response = requests.get(csv_url)
 response.raise_for_status()  
@@ -25,15 +24,14 @@ def get_chart():
     start_date = request.args.get('start', '')
     end_date = request.args.get('end', '')
 
-    # 确保提供了有效的特征名
+
     if feature not in df_features_scores.columns:
         return "Invalid feature specified", 400
 
-    # 处理可能的空日期参数
+
     start = pd.to_datetime(start_date, errors='coerce', utc=True) if start_date else None
     end = pd.to_datetime(end_date, errors='coerce', utc=True) if end_date else None
 
-    # 确保索引是排序的
     df_sorted = df_features_scores.sort_index()
 
     if start is not None and end is not None:
@@ -41,10 +39,10 @@ def get_chart():
     else:
         filtered_data = df_sorted
 
-    # 对指定特征进行每月重采样，计算均值
+
     monthly_mean = filtered_data[feature].resample('M').mean()
 
-    # 绘制图表
+
     plt.figure(figsize=(10, 6))
     monthly_mean.plot(title=feature)
     plt.ylabel(feature)
